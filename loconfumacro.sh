5c26f89dff5aad485a66382a1c72131e4534a362
tagezi <lera.goncharuk@gmail.com>
2015-09-08 02:21:30 +0300

    v0.1 thinking

--- loconfumacro.sh
+++ loconfumacro.sh
@@ -2,9 +2,20 @@
 
 # extracted from scaddins/source/analysis
 # extracted from scaddins/source/datefunc
+<<<<<<< HEAD
 # extracted from scaddins/source/pricing
+=======
+# extracted from scaddins/source/pricing 
+>>>>>>> the start thinking
 # extracted from sc/source/ui/src
 
 #find ../translations/source/ru/ -type f -exec cat $* {} \; 2>/dev/null | grep -r 'useradminpage.ui'
 
+<<<<<<< HEAD
 grep -rh 'useradminpage.ui' ../translations/source/ru/
+=======
+grep -rh '\"SC_OPCODE' ../translations/source/ru/ | cut -c 2- | sed 's/...$//'
+grep -rh '\"DATE_FUNCNAME' ../translations/source/ru/ | cut -c 2- | sed 's/...$//'
+grep -rh '\"ANALYSIS_FUNCNAME' ../translations/source/ru/ | cut -c 2- | sed 's/...$//'
+grep -rh '\"PRICING_FUNCNAME' ../translations/source/ru/ | cut -c 2- | sed 's/...$//'
+>>>>>>> the start thinking
