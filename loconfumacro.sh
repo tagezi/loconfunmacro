d912cd24f565879515d4ad95b65acda5b15e5ebc
tagezi <lera.goncharuk@gmail.com>
2015-09-08 15:59:40 +0300

    v0.18

--- loconfumacro.sh
+++ loconfumacro.sh
@@ -5,9 +5,17 @@
 # extracted from scaddins/source/pricing
 # extracted from sc/source/ui/src
 
-grep -rhA2 '\"SC_OPCODE' ../translations/source/ru/ | 
-sed -e '/"string.text"/d' -e 's/msgid "//' -e 's/\\n\"//' -e 's/"//' -e '2~2d'  | 
-sed '/SC_OPCODE_ERROR/{n;p;}'
+#declare -a sc_opcode_fun
+
+#sc_opcode_fun=( $(grep -rhA2 '\"SC_OPCODE' ../translations/source/ru/ | 
+#sed -e '/\"string.text\"/d'  -e 's/msgid \"//' -e 's/\\n\"//' -e 's/\"//' -e '2~2d'))
+
+grep -rhA2 '\"SC_OPCODE' ../translations/source/ru/ | sed -e '/\"string.text\"/d'  -e 's/msgid \"//' -e 's/\\n\"//' -e 's/\"//' -e '2~2d' |
+sed -e '/SC_OPCODE_ERROR_T/!s/SC_OPCODE_ERROR/#SC_OPCODE_ERROR/g' -e '/ *#/d' -e '/SC_OPCODE_TABLE/d' |
+sed -e '1!G;h;$!d' #переворачиваем сторки
+
 #grep -rh '\"DATE_FUNCNAME' ../translations/source/ru/ | cut -c 2- | sed 's/...$//'
 #grep -rh '\"ANALYSIS_FUNCNAME' ../translations/source/ru/ | cut -c 2- | sed 's/...$//'
 #grep -rh '\"PRICING_FUNCNAME' ../translations/source/ru/ | cut -c 2- | sed 's/...$//'
+
+#echo ${sc_opcode_fun[@]}
