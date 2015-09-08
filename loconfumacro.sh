ba4756f0907dbd550c0122e23eb6e5a8bb3146c2
tagezi <lera.goncharuk@gmail.com>
2015-09-08 13:04:04 +0300

    v0.17

--- loconfumacro.sh
+++ loconfumacro.sh
@@ -5,7 +5,9 @@
 # extracted from scaddins/source/pricing
 # extracted from sc/source/ui/src
 
-grep -rhA2 '\"SC_OPCODE' ../translations/source/ru/ | sed -e '/"string.text"/d' -e 's/msgid "//' -e 's/\\n\"//' -e 's/"//' -e '2~2d'  | sed '/SC_OPCODE_ERROR/{n;p;}' #
+grep -rhA2 '\"SC_OPCODE' ../translations/source/ru/ | 
+sed -e '/"string.text"/d' -e 's/msgid "//' -e 's/\\n\"//' -e 's/"//' -e '2~2d'  | 
+sed '/SC_OPCODE_ERROR/{n;p;}'
 #grep -rh '\"DATE_FUNCNAME' ../translations/source/ru/ | cut -c 2- | sed 's/...$//'
 #grep -rh '\"ANALYSIS_FUNCNAME' ../translations/source/ru/ | cut -c 2- | sed 's/...$//'
 #grep -rh '\"PRICING_FUNCNAME' ../translations/source/ru/ | cut -c 2- | sed 's/...$//'
