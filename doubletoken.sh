43ed77bf06724b3fb56e14705739f5de3b5d161f
tagezi <lera.goncharuk@gmail.com>
2015-09-17 05:21:37 +0300

    for search the double token in Help

---
+++ doubletoken.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+###################################################################
+## You may distribute or modify it under the terms of either the ##
+## GNU LESSER GENERAL PUBLIC LICENSE Version 3.0 or later,       ##
+## or Mozilla Public License Version 2.0 or later.               ##
+###################################################################
+
+testLIST=( $(cat list.log)); 
+i=2;
+while [[ ${testLIST[$i]} != "" ]]
+	do 
+	grep -r "\"${testLIST[$i]}\"" ../helpcontent2/source/text/scalc/
+	
+	(( i++))
+	done > list0.log
+
+exit 0
