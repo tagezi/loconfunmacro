7652aa348bac011fcd493c5804762a93c2a63861
tagezi <lera.goncharuk@gmail.com>
2016-10-31 02:55:43 +0200

    no much change for end
    now work all function except Convert and those that is not translated
    qtz still is made automaticaly
    there is som cosmetic changes

--- loconfuscripts.sh
+++ loconfuscripts.sh
@@ -60,16 +60,16 @@ sc_opcode_fun=( $(grep -rhA2 '\"SC_OPCODE' "$resource" |
                 sed -e '/ *#/d;/SC_OPCODE_TABLE_REF/d;/SC_OPCODE_NO_NAME/d'))
 
 #EN:Make a selection of functions and codes from the scaddins/source/datefunc.po file
-#date_funcname_fun=( $(grep -rhA2 '\"DATE_FUNCNAME' "$datefunc" | 
-#               sed -e '/\"string.text\"/d'  -e 's/msgid \"//;s/\\n\"//;s/\"//' -e '2~2d' ))
+date_funcname_fun=( $(grep -rhA2 '\"DATE_FUNCNAME' "$datefunc" | 
+               sed -e '/\"string.text\"/d'  -e 's/msgid \"//;s/\\n\"//;s/\"//' -e '2~2d' ))
 
 #EN:Make a selection of functions and codes from the scaddins/source/analysis.po file
-#analysis_fun=( $(grep -rhA2 '\"ANALYSIS_FUNCNAME' "$analysis" | 
-#               sed -e '/\"string.text\"/d'  -e 's/msgid \"//;s/\\n\"//;s/\"//' -e '2~2d' ))
+analysis_fun=( $(grep -rhA2 '\"ANALYSIS_FUNCNAME' "$analysis" | 
+               sed -e '/\"string.text\"/d'  -e 's/msgid \"//;s/\\n\"//;s/\"//' -e '2~2d' ))
 
 #EN:Make a selection of functions and codes from the scaddins/source/pricing.po file
-#pricing_fun=( $(grep -rhA2 '\"PRICING_FUNCNAME' "$pricing" |
-#               sed -e '/\"string.text\"/d'  -e 's/msgid \"//;s/\\n\"//;s/\"//' -e '2~2d' ))
+pricing_fun=( $(grep -rhA2 '\"PRICING_FUNCNAME' "$pricing" |
+               sed -e '/\"string.text\"/d'  -e 's/msgid \"//;s/\\n\"//;s/\"//' -e '2~2d' ))
 
 echo "sampling function names is made"
 
@@ -89,8 +89,8 @@ while [[ ${arrayFUN[$i]} != "" ]]
     if [[ ${arrayFUN[$i]} == "NEG"          || ${arrayFUN[$i]} == "MULTIPLE.OPERATIONS" ]] ||
        [[ ${arrayFUN[$i]} == "MULTIRANGE"   || ${arrayFUN[$i]} == "GOALSEEK"            ]] ||
        [[ ${arrayFUN[$i]} == "MVALUE"       || ${arrayFUN[$i]} == "DURATION"            ]] ||
-       [[ ${arrayFUN[$i]} == "EFFECTIVE"    ]]
-        then 
+       [[ ${arrayFUN[$i]} == "EFFECTIVE"    || ${arrayFUN[$i]} == "WEEKNUM_OOO"         ]]
+    then 
         (( i++ ))
         tokenFUN=${arrayFUN[$i]}
         (( i++ ))
@@ -99,36 +99,54 @@ while [[ ${arrayFUN[$i]} != "" ]]
     strNAME=${arrayFUN[$i]}
     
     case "${tokenFUN:0:9}" in
-        "SC_OPCODE" ) strQTZ=$( grep -h "‖${strNAME/\./\\\.}\"" "$core_resource")
+        "SC_OPCODE" ) strQTZ=$( grep -h "‖${strNAME/\./\\\.}\"" "$core_resource" | sed 's/^[ \t]*Text\[ qtz \] = \"//;s/..$//')
                 strCAT=$( grep -A17 "$tokenFUN"'$' "$scfuncs" | sed 's/^[ \t]*//;s/.$//'   | grep "ID_FUNCTION")
                 strNMDG=$(grep -A17 "$tokenFUN"'$' "$scfuncs" | sed 's/^[ \t]*//;s/.$//' | grep -A1 "ID_FUNCTION_GRP" | grep "HID")
             #    strNMDG=${strNMDG:5}
                 file_tac=$( grep -A17 "$tokenFUN"'$' "$scfuncs" | sed 's/^[ \t]*//;s/..$//'  | grep "qtz");;
-        "ANALYSIS_" ) strQTZ=$( grep -h "‖$strNAME\"" "$file_analysis")
+        "ANALYSIS_" ) strQTZ=$( grep -h "‖$strNAME\"" "$file_analysis" | sed 's/^[ \t]*Text\[ qtz \] = \"//;s/..$//')
                 strCAT=$( grep $( sed 's/\(\S\+\)/\L\u\1/;s/\(\S\+[A-Za-z]\)2\(\S\+[A-Za-z]\)/\L\u\1\E2\L\u\2/' \
                     <<< $strNAME)"," $file_canalysis | grep -o "FDCat.*[A-Za-z]")
                 strNMDG="ANALYSIS_${tokenFUN:18}"
                 file_tac=$(sed "1,/$strNMDG/ d;/}.$/,$ d;s/^[ \t]*//;s/..$//" "$file_danalysis" | grep "qtz" )
                 strNMDG=$( tr "[a-z]" "[A-Z]" <<< "HID_AAI_FUNC_${tokenFUN:18}");;
-        "DATE_FUNC" ) strQTZ=$( grep -h "‖$strNAME\"" "$file_datefunc" | sed 's/^[ \t]*//;' )
+        "DATE_FUNC" ) strQTZ=$( grep -h "‖$strNAME\"" "$file_datefunc" | sed 's/^[ \t]*Text\[ qtz \] = \"//;s/..$//' )
                 if [ $strNAME == "ROT13" ]; then strCAT="Text"; else strCAT="Date&Time"; fi 
                 strNMDG="DATE_FUNCDESC_${tokenFUN:14}"
                 file_tac=$(sed "1,/$strNMDG/ d;/}.$/,$ d;s/^[ \t]*//;s/..$//" "$file_datefunc"| grep "qtz" )
                 strNMDG=$( tr "[a-z]" "[A-Z]" <<< "HID_DAI_FUNC_${tokenFUN:14}");;
-        "PRICING_F" ) strQTZ=$(grep -h "‖$strNAME\"" "$file_pricing" | sed 's/^[ \t]*//;' )
+        "PRICING_F" ) strQTZ=$(grep -h "‖$strNAME\"" "$file_pricing" | sed 's/^[ \t]*Text\[ qtz \] = \"//;s/..$//')
                 strCAT="Financial"
                 strNMDG="PRICING_FUNCDESC_${tokenFUN:17}"
                 file_tac=$(sed "1,/$strNMDG/ d;/}.$/,$ d;s/^[ \t]*//;s/..$//" "$file_pricing" | grep "qtz" );;
     esac
     
-    strQTZ=${strQTZ:17:5}
+    strQTZ=${strQTZ:0:5}
     strKEY=${file_tac:15:5}
     strDG=${file_tac:21}
     strHELP=$(grep -rhA8 "$strNMDG\"" $helpcontent | grep "ahelp hid" | 
             sed -e :a -e 's/<[^>]*>//g;/</N;//ba;s/^[ \t]*//;' | sed '/Syntax/,$d' | tr -d '\n')
     
-    if [[ $strNAME == "CONVERT" ]]; then echo "$strNAME -- $strCAT  --  $strNMDG -- $strHELP"; fi
-    
+    if [[ $strCAT == "FDCat_Math, \"_EXCEL" && $strNAME == "GCD" ]]
+    then
+        strCAT="FDCat_Math"
+        strNAME="GCD_EXCEL2003"
+    elif [[ $strCAT == "FDCat_Math, \"_EXCEL" && $strNAME == "LCM" ]]
+    then
+        echo "$strNAME | ${strQTZ} | ${tokenFUN} | $strCAT | ${strDG} | ${strKEY} | ${strNMDG} | $strHELP"
+        strCAT="FDCat_Math"
+        strNAME="LCM_EXCEL2003"
+    elif [[ $strCAT == "FDCat_DateTime, \"_EXCEL" && $strNAME == "NETWORKDAYS" ]]
+    then
+        strCAT="FDCat_DateTime"
+        strNAME="NETWORKDAYS_EXCEL2003"
+    elif [[ $strCAT == "FDCat_DateTime, \"_EXCEL" && $strNAME == "WEEKNUM" ]]
+    then
+        strCAT="FDCat_DateTime"
+        strNAME="WEEKNUM_EXCEL2003"
+    fi
+
+   # if [[ $strNAME == "GCD*" ]]; then echo " $strNAME -- $strCAT "; fi  
     case "$strCAT" in
         "ID_FUNCTION_GRP_STATISTIC" )   strCAT="Statistic";;
         "ID_FUNCTION_GRP_MATH" )        strCAT="Mathematical";;
@@ -151,7 +169,7 @@ while [[ ${arrayFUN[$i]} != "" ]]
     esac
     
     #EN:Write CSV ans Wiki files
-    echo "$strNAME|${strQTZ}|${tokenFUN}|$strCAT|${strDG}|${strKEY}|${strNMDG}|$strHELP|" >> tmp_file
+    echo "${strNAME}|${strQTZ}|${tokenFUN}|$strCAT|${strDG}|${strKEY}|${strNMDG}|$strHELP|" >> tmp_file
     (( i++ ))
 
     done
